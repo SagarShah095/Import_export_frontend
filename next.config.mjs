11394642692af13cc3a8f711ea49309b8c649862
coderb <coderb@coderbs-Mac-mini.local>
2026-04-15 09:47:01 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.gstatic.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
